c14d26e3a382c06fda077dd6c90aaaf270d9338b
baggariSiva <narayana@softsuave.com>
2022-05-19 00:03:40 +0530

    Fixed the issues with image loading

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,25 @@
+const path = require(`path`);
 module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sass`,
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        icon: 'src/images/elestio-logo.svg',
+        icon: 'src/images/favicoon.png',
       },
     },
-
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: path.join(__dirname, `src`, `images`),
+      },
+    },
+    
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,112 +1,102 @@
-import axios from "axios"
-import React, { useState, useEffect } from "react"
-import {
-  container,
-  appbody,
-  appheading,
-  appinfoblock,
-  subVal,
-  appdeploy,
-  area,
-  circles,
-  btn,
-  listCircles,
-  h1,
-  h4,
-  appheader
-} from "./layout.module.css"
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import Image from "../components/image";
+import "../components/style.scss";
 
 export default function Home() {
-  const [ip, setIP] = useState("?")
-  const [city, setCity] = useState("?")
-  const [country, setCountry] = useState("?")
-  const [host, setHost] = useState("")
-  const [Latency, setLatency] = useState("?")
+  const [ip, setIP] = useState("?");
+  const [city, setCity] = useState("?");
+  const [country, setCountry] = useState("?");
+  const [host, setHost] = useState("");
+  const [Latency, setLatency] = useState("?");
 
-   const getlatency=()=>{
-    var started=new Date().getTime();
-    var url="https://ipinfo.io/json?t=" + (+new Date());
-    fetch(url).then(function(response){
-      var ended=new Date().getTime();
-      var milliseconds=ended - started;
-      setLatency(milliseconds);
-    }).catch(function(error){
-      setLatency("?")
-    })
-  }
+  const getlatency = () => {
+    var started = new Date().getTime();
+    var url = "https://ipinfo.io/json";
+    fetch(url)
+      .then(function (response) {
+        var ended = new Date().getTime();
+        var milliseconds = ended - started;
+        setLatency(milliseconds);
+      })
+      .catch(function (error) {
+        setLatency("?");
+      });
+  };
 
   const getData = async () => {
-    const hname = window.location.hostname
-    setHost(hname)
-    setInterval(getlatency,1000)
-    const res = await axios.get("https://ipinfo.io/json")
+    const hname = window.location.hostname;
+    setHost(hname);
+    setInterval(getlatency, 1000);
+    const res = await axios.get("https://ipinfo.io/json");
     setCity(res.data.city);
     setCountry(res.data.country);
     setIP(res.data.ip);
-    
-  }
+  };
 
   useEffect(() => {
-    getData()
-  }, [])
+    getData();
+  }, []);
 
   return (
-    <main className={container}>
-      <title>Elestio Staic Page</title>
-      <div className={appheader}>
-        <img src="src/images/elestio-logo.svg"  alt="logo"/>
-    </div>
-      <div className={appbody}>
-        <div className={appheading}>
-          <h1 className={h1}>Welcome to Elestio</h1>
-          <h4 className={h4}>
-            Deploy your apps quickly with the easiest CI/CD system
-          </h4>
+    <main>
+      <title>Elestio Staic Example</title>
+      <Image
+        src="elestio-logo.svg"
+        alt="logo"
+        style={{ height: "40px", margin: "20px" }}
+      />
+      <div className="container">
+        <div className="app-body">
+          <div className="app-heading">
+            <h1>Welcome to Elestio</h1>
+            <h4>Deploy your apps quickly with the easiest CI/CD system</h4>
+          </div>
+
+          <p className="app-info-block">
+            This Host <strong className="subVal">{host}</strong>
+          </p>
+
+          <p className="app-info-block">
+            Your IP <strong className="subVal">{ip}</strong>
+          </p>
+
+          <p className="app-info-block">
+            Your Location
+            <strong className="subVal">
+              {country},{city}
+            </strong>
+          </p>
+
+          <p className="app-info-block">
+            Latency to server <strong className="subVal">{Latency} ms</strong>
+          </p>
+
+          <div className="app-deploy">
+            <a
+              href="https://dash.elest.io/deploy?source=cicd&social=Github&url=https://github.com/elestio-examples/static"
+              className="btn"
+            >
+              Deploy on Elestio
+            </a>
+          </div>
         </div>
 
-        <p className={appinfoblock}>
-          This Host <strong className={subVal}>{host}</strong>
-        </p>
-
-        <p className={appinfoblock}>
-          Your IP <strong className={subVal}>{ip}</strong>
-        </p>
-
-        <p className={appinfoblock}>
-          Your Location
-          <strong className={subVal}>
-            {country},{city}
-          </strong>
-        </p>
-
-        <p className={appinfoblock}>
-          Latency to server <strong className={subVal}>{Latency} ms</strong>
-        </p>
-
-        <div className={appdeploy}>
-          <a
-            href="https://dash.elest.io/deploy?source=cicd&social=Github&url=https://github.com/elestio-examples/static"
-            className={btn}
-          >
-            Deploy on Elestio
-          </a>
+        <div className="area">
+          <ul className="circles">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
         </div>
-      </div>
-
-      <div className={area}>
-        <ul className={circles}>
-          <li className={listCircles}></li>
-          <li className={listCircles}></li>
-          <li className={listCircles}></li>
-          <li className={listCircles}></li>
-          <li className={listCircles}></li>
-          <li className={listCircles}></li>
-          <li className={listCircles}></li>
-          <li className={listCircles}></li>
-          <li className={listCircles}></li>
-          <li className={listCircles}></li>
-        </ul>
       </div>
     </main>
-  )
+  );
 }
